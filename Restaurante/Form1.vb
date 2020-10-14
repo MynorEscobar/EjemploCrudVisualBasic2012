f65c8228f0890dc3aef96a2ea652403b38b7a0e4
MynorEscobar <mynorescobarefpem@gmail.com>
2020-10-14 17:42:20 -0600

    conexion a base de datos (ModConex.vb) y proceso de busqueda de cliente por medio de nit

---
+++ Restaurante/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+
+    
+    Private Sub btnBuscarCliente_Click(sender As Object, e As EventArgs) Handles btnBuscarCliente.Click
+        abrirConexion()
+
+        Dim codigob As String
+        codigob = "select * from clientes where nit='" & txtNit.Text & "'"
+
+        Dim ejecodigob As New OleDb.OleDbCommand
+        ejecodigob.CommandType = CommandType.Text
+        ejecodigob.Connection = conec
+        ejecodigob.CommandText = codigob
+
+        Dim guardareje As OleDb.OleDbDataReader
+        guardareje = ejecodigob.ExecuteReader
+        guardareje.Read()
+        If guardareje.HasRows Then
+            txtNombre.Text = guardareje!nombre
+            txtDireccion.Text = guardareje!nit
+        Else
+            MsgBox("codigo no existe")
+        End If
+        guardareje.Close()
+
+        cerrarConexion()
+
+    End Sub
+End Class
---
+++ Restaurante/ModConexion.vb
@@ -0,0 +1,18 @@
+﻿Module ModConexion
+    Public conec As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Restaurante.accdb")
+    Public Sub abrirConexion()
+        Try
+            conec.Open()
+            MsgBox("conexion realizada con exito")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Public Sub cerrarConexion()
+        Try
+            conec.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Module
